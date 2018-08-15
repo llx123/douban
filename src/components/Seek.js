4135fd8d9f292ef0b6cfba2b138dde06a5655fd4
llx123 <573381010@qq.com>
2018-08-15 23:34:59 +0800

    临时seek

---
+++ src/components/Seek.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native'
+
+
+export default class HotList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          This is Seek!
+        </Text>
+      </View>
+    )
+  }
+}
