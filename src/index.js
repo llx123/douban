93534bf12a973aa2416ab1cfde211214bf1b6def
llx123 <573381010@qq.com>
2018-08-20 22:18:26 +0800

    安卓新屏幕右侧划入配置

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import {
+  Easing,
+  Animated
+} from 'react-native'
 
 import Icon from "react-native-vector-icons/Ionicons"
 
@@ -65,6 +69,35 @@ const MyApp = createStackNavigator({
   Detail,
   Search
 }, {
-    headerMode: 'screen'
+    headerMode: 'screen',
+    // headerMode: 'none',
+    mode: 'modal',
+    navigationOptions: {
+      gesturesEnabled: false,
+    },
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing,
+      },
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
+
+        const width = layout.initWidth;
+        const translateX = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [width, 0, 0],
+        });
+
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        });
+
+        return { opacity, transform: [{ translateX }] };
+      },
+    }),
   });
 export default MyApp
