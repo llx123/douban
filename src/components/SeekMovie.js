ca7eb71936ae0f68fb27b0cb2f9610d8936483e5
llx123 <573381010@qq.com>
2018-08-24 17:34:30 +0800

    async/await接口

--- src/components/SeekMovie.js
+++ src/components/SeekMovie.js
@@ -20,25 +20,30 @@ export default class SeekMovie extends Component {
     super(props)
     this.state = {
       ready: true,
-      hotData: [], //热门
+      allData: [], //热门
     }
   }
   componentDidMount() {
     this.fetchData()
   }
-  fetchData() {
-    fetch(`${baseUri}/in_theaters`)
-      .then(res => {
-        return res.json(); // 正在上映        
-      }).then(res => {
-        this.setState({
-          hotData: res.subjects,
-          ready: false
-        })
-      })
+  fetchData = async () => {
+    let hotData = await this.fetchHot()
+    this.setState({
+      allData: [{ title: '', data: [{ data: hotData }]}],
+      ready: false
+    })
+  }
+  fetchHot = async () => { // 电影热榜    
+    try {
+      let response = await fetch(`${baseUri}/in_theaters`);
+      let responseJson = await response.json();
+      return responseJson.subjects;
+    } catch (error) {
+      console.error(error);
+    }
   }
   render() {
-    const { hotData } = this.state
+    const { allData } = this.state
     return (
       <View style={styles.seekMovie}>
         {this.state.ready
@@ -48,28 +53,42 @@ export default class SeekMovie extends Component {
               <Text>111</Text>
               <Text>222</Text>
             </View>
-            <ScrollView
-              contentContainerStyle={styles.contentContainer}
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-            >
-              {hotData.map((item, index) => {
+            <SectionList
+              sections={allData}
+              keyExtractor={(item, index) => item + index}
+              renderSectionHeader={({ section: { title } }) => (
+                <Text style={styles.sectionHeader}>{title}</Text>
+              )}
+              renderItem={({ item, index, section }) => {
                 return (
-                  <View style={styles.hotItem} key={index}>
-                    <Image source={{
-                      uri: item.images.large
-                    }} style={{
-                      width: 80,
-                      height: 100
-                    }} />
-                    <Text style={styles.smallFont}>{item.title.slice(0,6)}{item.title.length>6?index:index}</Text>
-                    <View style={styles.star}>
-                      <Star value={item.rating.stars} />
-                      {item.rating.stars > 0 && <Text style={styles.smallFont}>{(item.rating.stars / 10).toFixed(1) + '分'}</Text>}
-                    </View>
-                  </View>)
-              })}
-            </ScrollView>
+                  <View key={index.toString()}>
+                    {index === 0 && <ScrollView
+                      contentContainerStyle={styles.contentContainer}
+                      horizontal={true}
+                      showsHorizontalScrollIndicator={false}
+                    >
+                      {item.data.map((item, index) => {
+                        return (
+                          <View style={styles.hotItem} key={index.toString()+''+item.title}>
+                            <Image source={{
+                              uri: item.images.large
+                            }} style={{
+                              width: 80,
+                              height: 100
+                            }} />
+                            <Text style={styles.smallFont}>{item.title.slice(0, 6)}{item.title.length > 6 ? index : index}</Text>
+                            <View style={styles.star}>
+                              <Star value={item.rating.stars} />
+                              {item.rating.stars > 0 && <Text style={styles.smallFont}>{(item.rating.stars / 10).toFixed(1) + '分'}</Text>}
+                            </View>
+                          </View>)
+                      })}
+                    </ScrollView>}                    
+                  </View>
+                )
+              }}
+            />
+            
           </View>}
       </View>
     )
@@ -90,15 +109,14 @@ const styles = StyleSheet.create({
     paddingVertical: 20
   },
   contentContainer: {
-    width: width*3,
-    height: 800,
+    width: width * 3,
     flexDirection: 'row',
     flexWrap: 'wrap',
     paddingVertical: 20,
   },
   hotItem: {
     width: 110,
-    flexDirection:'column',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -108,7 +126,7 @@ const styles = StyleSheet.create({
     fontSize: 10
   },
   star: {
-    display:'flex',
+    display: 'flex',
     flexDirection: 'row',
     marginTop: 3,
     marginBottom: 3,
