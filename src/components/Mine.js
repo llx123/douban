228ecf855b3ee8068394b76711fc8314719ebc16
llx123 <573381010@qq.com>
2018-09-04 22:29:20 +0800

    “我的”米快

--- src/components/Mine.js
+++ src/components/Mine.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import {
   View,
   Dimensions,
+  StyleSheet,
+  Text,
   Image
 } from 'react-native';
+
+import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view';
+
 const { width, height } = Dimensions.get('window');
 
 export default class My extends Component {
@@ -22,12 +27,80 @@ export default class My extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Image source={require('../img/headImg.jpg')} style={{
-          width: 200,
-          height: 200
-        }} />
+      <View style={styles.container}>
+        <View style={styles.top}>
+          <View style={styles.topImg}>
+            <Image source={require('../img/headImg.jpg')} style={{width: 60, height: 60}}/>
+          </View>
+          <Text style={{fontSize: 18, color: '#fff'}}>请登录</Text>
+        </View>
+        <View style={styles.bottom}>
+          <ScrollableTabView
+            renderTabBar={() => <DefaultTabBar />}
+            tabBarUnderlineStyle={{
+              backgroundColor: '#000',
+              height: 2
+            }}
+            tabBarBackgroundColor='#FFFFFF'
+            tabBarActiveTextColor='#000'
+            tabBarInactiveTextColor='#959595'
+            tabBarTextStyle={{ fontSize: 14 }}
+            locked={false}
+          >
+            <View tabLabel='讨论' style={styles.scrollView}>
+              <Image source={require('../img/headImg.jpg')} style={styles.headImg}/>
+            </View>
+            <View tabLabel='想看' style={styles.scrollView}>
+              <Image source={require('../img/headImg.jpg')} style={styles.headImg}/>
+            </View>
+            <View tabLabel='在看' style={styles.scrollView}>
+              <Image source={require('../img/headImg.jpg')} style={styles.headImg}/>
+            </View>
+            <View tabLabel='看过' style={styles.scrollView}>
+              <Image source={require('../img/headImg.jpg')} style={styles.headImg}/>
+            </View>
+            <View tabLabel='影评' style={styles.scrollView}>
+              <Image source={require('../img/headImg.jpg')} style={styles.headImg}/>
+            </View>
+            <View tabLabel='影人' style={styles.scrollView}>
+              <Image source={require('../img/headImg.jpg')} style={styles.headImg}/>
+            </View>
+          </ScrollableTabView>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  top: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'green'
+  },
+  topImg: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderColor: '#fff',
+    marginHorizontal: 30,
+    overflow: 'hidden'
+  },
+  bottom: {
+    flex: 2
+  },
+  scrollView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  headImg: {
+    width: 150,
+    height: 150
+  }
+})
